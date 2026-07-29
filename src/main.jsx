f7a6414a14a28234e484fd0c5a960ca73e1565de
chirpy518-star <chirpy518@gmail.com>
2026-07-29 14:35:34 +0530

    Integrate Redux Provider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+import App from "./App";
+import { store } from "./app/store";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
