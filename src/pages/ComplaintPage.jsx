3c826db34876d4b953f13f14c3c51283fe234b5d
chirpy518-star <chirpy518@gmail.com>
2026-07-29 14:30:20 +0530

    Refactor ComplaintPage component structure

--- src/pages/ComplaintPage.jsx
+++ src/pages/ComplaintPage.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import ComplaintForm from '../components/ComplaintForm';
-import ComplaintAssistant from '../components/ComplaintAssistant';
-import '../styles/complaint.css';
+import ComplaintForm from "../components/ComplaintForm";
+import ComplaintAssistant from "../components/ComplaintAssistant";
+import "../styles/complaint.css";
 
-export default function ComplaintPage() {
-  return (
-    <main className="complaint-page">
-      <h1>Complaint</h1>
-      <div className="complaint-grid">
-        <div className="left">
-          <ComplaintAssistant suggestion="Try to include order numbers or screenshots if available." />
+export default function ComplaintPage(){
+
+    return(
+
+        <div className="page">
+
+            <ComplaintForm/>
+
+            <ComplaintAssistant/>
+
         </div>
-        <div className="right">
-          <ComplaintForm />
-        </div>
-      </div>
-    </main>
-  );
+
+    );
+
 }
